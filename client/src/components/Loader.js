fc0f3f512db985627d846e3abe5440300a80388c
Ayush-Kumar0 <ayushkumar27053@gmail.com>
2023-06-02 19:22:35 +0530

    fixed-errors

--- client/src/components/Loader.js
+++ client/src/components/Loader.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import loader from "../../public/assets/images/loader.gif";
 
 function Loader() {
     return (
         <div style={{ width: '25%', minHeight: 'calc(100vh - 60px)', display: 'flex', flexDirection: 'column', justifyContent: 'center', margin: 'auto' }}>
-            <img src={loader} alt="loading" className="loader" />
+            <img src="assets/images/loader.gif" alt="loading" className="loader" />
         </div>
     );
 };
